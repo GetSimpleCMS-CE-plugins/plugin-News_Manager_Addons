66ff26ee5b7461867b3082b5398a916b08387d95
cnb <carnav@gmail.com>
2013-02-11 19:50:49 +0100

    add $before param to nm_list_recent_with_date
    lets you specify position of date respect the title

--- news_manager_addons.php
+++ news_manager_addons.php
@@ -5,7 +5,7 @@ $thisfile = basename(__FILE__, ".php");
 register_plugin(
 	$thisfile,
 	'News Manager Addons',
-	'0.1 beta',
+	'0.1.1 beta',
 	'Carlos Navarro',
 	'http://www.cyberiada.org/cnb/',
 	'Additional functions/template tags for News Manager'
@@ -14,8 +14,10 @@ register_plugin(
 /*******************************************************
  * @function nm_list_recent_with_date
  * @action print a list with the latest posts (titles and dates)
+ * @param $fmt date format (strftime)
+ * @param $before show date before (true) or after (false, default) the post title
  */
-function nm_list_recent_with_date($fmt='') {
+function nm_list_recent_with_date($fmt='', $before=false) {
   global $NMRECENTPOSTS;
   if ($fmt == '') $fmt = i18n_r('news_manager/DATE_FORMAT');
   $posts = nm_get_posts();
@@ -26,7 +28,11 @@ function nm_list_recent_with_date($fmt='') {
       $url = nm_get_url('post').$post->slug;
       $title = stripslashes($post->title);
       $date = nm_get_date($fmt, strtotime($post->date));
-      echo "<li><a href=\"$url\">$title</a> $date</li>";
+      if ($before) {
+        echo '<li>',$date,' <a href="',$url,'">',$title,'</a></li>';
+      } else {
+        echo '<li><a href="',$url,'">',$title,'</a> ',$date,'</li>';
+      }
     }
     echo '</ul>';
   }
